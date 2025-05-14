ba5436a68f8b5f8ce315c54eb41ec8f6016f5712
MrPravinS <shegamwarpravin07@gmail.com>
2025-05-14 21:23:23 +0530

    new feature added

---
+++ gitAndGithub/constant/base.js
@@ -0,0 +1,13 @@
+function getName(name)
+{
+    console.log(`Hey Your name is: ${name}`);
+    
+}
+
+getName("Pravin")
+
+
+function newFeatureAdded(){
+    console.log("New Feature added");
+    
+}
